3e5346befcdfbbdc0f0166f7c95782a3559bcd40
B10215029 <B10215029@mail.ntust.edu.tw>
2018-06-19 18:54:41 +0800

    add readme and remove clr input

--- main.cpp
+++ main.cpp
@@ -8,18 +8,5 @@ int main() {
 	srand(time(NULL));
 	RubiksCubeSolver::RubiksCubeSimulator form;
 	System::Windows::Forms::Application::Run(%form);
-	RubiksCube cube(3);
-	std::cout << cube;
-	int type;
-	int column;
-	int angle;
-	
-	while (true) {
-		std::cin >> type >> column >> angle;
-		cube.Rotate(type, column, angle);
-		std::cout << cube;
-	}
-	//cube.Rotate(RubiksCube::OperateType::B);
-	system("pause");
 	return 0;
 }
